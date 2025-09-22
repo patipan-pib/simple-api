ad96b6cf8f94c1093eeebc855dbfcbada5d63ac9
By_Pib <ddpatipan@gmail.com>
2025-09-22 23:59:16 +0700

    app.py

--- app/app.py
+++ app/app.py
@@ -9,13 +9,10 @@ GETCODE = "TEST SUCCESS"
 def index():
     return "Index!"
 
-@app.route('/hello/<name>', methods=['GET'])
-def hello(name):
-    return "Hello, " + str(name)
 
 @app.route('/getcode', methods=['GET'])
 def getcode():
-    return GETCODE
+    return jsonify({ 'code': GETCODE })
 
 @app.route('/plus/<num1>/<num2>', methods=['GET'])
 def plus(num1, num2):
@@ -28,23 +25,6 @@ def plus(num1, num2):
 
     return jsonify(result)
 
-
-@app.route('/calculate/<num1>/<num2>', methods=['GET'])
-def calculate(num1, num2):
-    try:
-        num1 = eval(num1)
-        num2 = int(num2)
-
-        results = {
-                'plus' : num1 + num2,
-                'minus' : num1 - num2,
-                'multiply': num1 * num2,
-                'divide' : num1/num2
-            }
-    except:
-        results = { 'error_msg' : 'inputs must be numbers' }
-
-    return jsonify(results)
 
 
 if __name__ == '__main__':
